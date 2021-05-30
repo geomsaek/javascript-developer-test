f6afdf4d68add94c6db754c212d809e22f512764
geomsaek <msaliba.6@gmail.com>
2021-05-30 18:09:40 +1000

    commit test for arnie

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
+const AFFIRMATIVE = "Arnie Quote";
+const NEGATIVE = "FAILURE";
+
+const formatData = (data) => {
+
+  let obj = {};
+  const message = JSON.parse(data.body);
+  const keys = data.status === 200 ? AFFIRMATIVE : NEGATIVE;
+
+  obj[`${keys}`] = message.message;
+  return obj;
+
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+
+  let arnies = urls.map((item) => {
+    return httpGet(item).then((response) => formatData(response));
+  });
+
+  return Promise.all(arnies);
+
+}
 
 module.exports = {
   getArnieQuotes,
